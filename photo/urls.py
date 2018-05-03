e66c0d782e2aae92ba163ab55f1d0c2da5650a40
aquarius <haritonov.aka@gmail.com>
2018-05-03 20:36:17 +0300

    sitetree

--- photo/urls.py
+++ photo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AlbumList
+from .views import AlbumList, AlbumDetail
 
 urlpatterns = [
-    path('photos/', AlbumList.as_view(), name='album-list')
+    path('photos/', AlbumList.as_view(), name='album-list'),
+    path('photos/<int:pk>/', AlbumDetail.as_view(), name='album-detail'),
 ]
--- photo/views.py
+++ photo/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Album
 
 
@@ -6,3 +6,8 @@ class AlbumList(ListView):
     queryset = Album.objects.all()
     context_object_name = 'albums'
     template_name = 'photo/album_list.html'
+
+
+class AlbumDetail(DetailView):
+    model = Album
+    template_name = 'photo/album_detail.html'
