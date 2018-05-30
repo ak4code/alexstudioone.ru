1050395612d7e61b2db93f42074ec351d7e96667
aquarius <haritonov.aka@gmail.com>
2018-05-30 05:27:51 +0300

    front

--- alex_site/apps.py
+++ alex_site/apps.py
@@ -5,7 +5,3 @@ from django.apps import AppConfig
 class AlexSiteConfig(AppConfig):
     name = 'alex_site'
     verbose_name = 'Сайт'
-
-    def ready(self):
-        from .models import SiteConfiguration
-        config = SiteConfiguration.get_solo()
